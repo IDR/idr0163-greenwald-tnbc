21a340b9bc61bb33a6bb82d495235d6aa11b336d
Dominik Lindner <d.lindner@dundee.ac.uk>
2024-10-29 12:01:35 +0000

    Fix some issue in companion script

--- scripts/companions.py
+++ scripts/companions.py
@@ -7,14 +7,18 @@ import csv
 
 
 def get_dimensions(img_file, order="xyczt"):
-    img = tifffile.imread(img_file)
-    res = dict()
-    for i, d in enumerate(order):
-        if i < len(img.shape):
-            res[d] = img.shape[i]
-        else:
-            res[d] = 1
-    return res
+    try:
+        img = tifffile.imread(img_file)
+        res = dict()
+        for i, d in enumerate(order):
+            if i < len(img.shape):
+                res[d] = img.shape[i]
+            else:
+                res[d] = 1
+        return res
+    except Exception:
+        print(f"WARN: {img_file} not readable by tifffile!")
+        return None
 
 
 def write_companion(img, img_name, dir):
@@ -38,6 +42,54 @@ def get_channel_order(file):
             order[row['Channel']] = int(row['Order'])
         return order
 
+def process_img_dir(img_dir):
+    x = 0
+    y = 0
+    z = 1
+    c = 0
+    t = 1
+    files = []
+    for img_file in img_dir.iterdir():
+        if not img_file.is_file():
+            continue
+        dims = get_dimensions(img_file)
+        if not dims:
+            continue
+        img_name = img_dir.stem
+        img_file = f"{img_file.parent.name}/{img_file.name}"
+        if x == 0:
+            x = dims['x']
+        elif x != dims['x']:
+            print(f"Image {img_dir}/{img_file} has different dimensions!")
+            return False
+
+        if y == 0:
+            y = dims['y']
+        elif y != dims['y']:
+            print(f"Image {img_dir}/{img_file} has different dimensions!")
+            return False
+
+        if dims['c'] > 1:
+            print(f"Image {img_dir}/{img_file} has more than one channel!")
+            return False
+
+        files.append(img_file)
+        c += 1
+
+    img = Image(img_name, x, y, z, c, t)
+    for i, file in enumerate(files):
+        name = pathlib.Path(file).stem
+        if name not in channel_order:
+            print(f"WARN: Can't get channel index for {name}, excluding {file}.")
+            continue
+        channel_index = channel_order[name] - 1
+        img.add_channel(name=name)
+        img.add_plane(c=channel_index)
+        img.add_tiff(file, c=channel_index)
+
+    write_companion(img, img_name, out_dir)
+    return True
+
 
 if len(sys.argv) < 4:
     print("Usage: python companions.py IMAGE_ROOT_DIR CHANNEL_MAPPING.csv .")
@@ -51,43 +103,10 @@ out_dir = pathlib.Path(sys.argv[3])
 for img_dir in sample_dir.iterdir():
     if not img_dir.is_dir():
         continue
-    x = 0
-    y = 0
-    z = 1
-    c = 0
-    t = 1
-    files = []
-    for img_file in img_dir.iterdir():
-        if not img_file.is_file():
-            continue
-        dims = get_dimensions(img_file)
-        img_name = img_dir.stem
-        img_file = f"{img_file.parent.name}/{img_file.name}"
-        if x == 0:
-            x = dims['x']
-        elif x != dims['x']:
-            print(f"Image {img_dir}/{img_file} has different dimensions!")
-            break
-
-        if y == 0:
-            y = dims['y']
-        elif y != dims['y']:
-            print(f"Image {img_dir}/{img_file} has different dimensions!")
-            break
-
-        if dims['c'] > 1:
-            print(f"Image {img_dir}/{img_file} has more than one channel!")
-            break
-
-        files.append(img_file)
-        c += 1
-
-    img = Image(img_name, x, y, z, c, t)
-    for i, file in enumerate(files):
-        name = pathlib.Path(file).stem
-        channel_index = channel_order[name] - 1
-        img.add_channel(name=name)
-        img.add_plane(c=channel_index)
-        img.add_tiff(file, c=channel_index)
-
-    write_companion(img, img_name, out_dir)
+    try:
+        success = process_img_dir(img_dir)
+        if not success:
+            print(f"Skipped {img_dir} due to errors.")
+    except Exception as e:
+        print(f"{e}")
+        print(f"Skipped {img_dir} due to errors.")
